266b17c3c5c0fdd91f0fb7ff1dae4fdcc6a1b6c0
Aromal <aromalbabus404@gmail.com>
2026-07-02 21:06:14 +0530

    Add python-dotenv

--- aquacraft/settings.py
+++ aquacraft/settings.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import os
 import dj_database_url
 from dotenv import load_dotenv
-
 # -----------------------------------------------------------------------------
 # Base Directory
 # -----------------------------------------------------------------------------
